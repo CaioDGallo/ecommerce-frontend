b289819f6cac276ffe73d76d2148beeb53286502
Caio Delafiori Gallo <caiogallo88@gmail.com>
2020-06-28 17:21:00 -0300

    Setting up production url for the API

--- services/api.tsx
+++ services/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://ecommerce-backendv1.herokuapp.com'
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://ecommerce-backendv1.herokuapp.com'
+  //baseURL: 'http://localhost:3001'
 });
 
 export default api;
